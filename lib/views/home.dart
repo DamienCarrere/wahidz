a2a0ffca61200cbaf2aacbd8eb4196addb7a731b
Damien Carrère <125464208+DamienCarrere@users.noreply.github.com>
2025-11-23 09:29:19 +0100

    navbar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -15,27 +15,8 @@ class _HomeState extends State<Home> {
         title: Center(child: Text("HOME")),
         backgroundColor: Colors.orangeAccent,
       ),
+
       // body:
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.orange,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: "Catégories"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: "Panier",
-          ),
-        ],
-        onTap: (index) {
-          if (index == 0) {
-            Navigator.pushReplacementNamed(context, '/categories');
-          } else if (index == 1) {
-            Navigator.pushReplacementNamed(context, '/home');
-          } else if (index == 2) {
-            Navigator.pushReplacementNamed(context, '/cart');
-          }
-        },
-      ),
     );
   }
 }
