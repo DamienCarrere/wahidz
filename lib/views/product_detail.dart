0f62bc7f37fafbf9693973bfbc7825ee32f823ad
Lasdepic <clementjordan34@gmail.com>
2025-11-24 10:46:24 +0100

    correction

--- lib/views/product_detail.dart
+++ lib/views/product_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:wahidz/view_model/myCart.dart';
 import 'package:wahidz/view_model/product_vm.dart';
 import 'package:wahidz/view_model/myCart.dart';
 import 'package:wahidz/widgets/bottom_nav_bar.dart';
